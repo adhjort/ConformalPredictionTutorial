2b60b6b13f4fc6804264c545df59e6cf480f4aa4
Anders Hjort <adhjort@gmail.com>
2025-10-24 07:59:56 +0200

    Update part2_eletricity.R

--- code/part2_eletricity.R
+++ code/part2_eletricity.R
@@ -1,4 +1,4 @@
-source("./code/setup.R")
+source("setup.R")
 
 ######## DISCLAIMER ######## 
 # This code is highly inspired by the Python code used in the paper "Conformal prediction beyond exchangeability" by Barber et al. 
@@ -268,6 +268,7 @@ ggplot(data = alpha_df,
   geom_line() +
   theme_minimal(base_size = 14) +
   geom_hline(yintercept = 0.1, col = "red", lwd = 1)
+
 
 
 
